5b76f4774883e78ce386d78cc512e314589a2d1a
Sevastian Cañari <sevastianprime@outlook.com>
2023-10-26 22:48:46 -0500

    Envio de correo finalizado

---
+++ back-end/src/main/java/com/nocountry/inmuebles/controller/EnviarCorreo.java
@@ -0,0 +1,35 @@
+package com.nocountry.inmuebles.controller;
+
+import com.nocountry.inmuebles.service.impl.CorreoServiceImpl;
+import com.nocountry.inmuebles.util.ContactoCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/correo")
+public class EnviarCorreo {
+
+    @Autowired
+    CorreoServiceImpl correoService;
+
+    @PostMapping("/enviar")
+    public ResponseEntity<?> enviarCorreo(@RequestBody ContactoCliente cliente){
+        Map<String, String> salida = new HashMap<>();
+
+        Boolean resultado= correoService.enviarCorreo(cliente);
+        if(resultado==true){
+            salida.put("MENSAJE", "Se ha enviado el correo correctamente");
+        }else{
+            salida.put("MENSAJE", "Error en el envio del correo");
+        }
+        return ResponseEntity.ok(salida);
+    }
+}
