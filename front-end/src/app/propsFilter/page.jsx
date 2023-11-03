3dcd20767b4a2f6b33053f45633f5b992491f5b7
gonzasuarez96 <gonzasuarez100@gmail.com>
2023-11-03 04:30:35 -0300

    quite el buscador

--- front-end/src/app/propsFilter/page.jsx
+++ front-end/src/app/propsFilter/page.jsx
@@ -6,21 +6,19 @@ import NavFilter from "./components/navFilter";
 import { getSessionStorageLocation } from "../func/sessionStorage";
 
 export default function PropsFilterPage() {
-  const dt = JSON.parse(sessionStorage.getItem('location'));
-  const data = dt.ubic
-  console.log('dataPage:',data)
-  console.log(dt.cityDescription)
-  console.log(dt.provinceDescription)
-  const [propiedades, setPropiedades] = useState(data);
-
-
+  // const dt = JSON.parse(sessionStorage.getItem('location'));
+  // const data = dt.ubic
+  // console.log('dataPage:',data)
+  // console.log(dt.cityDescription)
+  // console.log(dt.provinceDescription)
+  // const [propiedades, setPropiedades] = useState(data);
 
   return (
     <div>
-      <NavFilter  setPropiedades={setPropiedades} data={ data } province={ dt.provinceDescription } city={ dt.cityDescription }/>
+      {/* <NavFilter  setPropiedades={setPropiedades} data={ data } province={ dt.provinceDescription } city={ dt.cityDescription }/>
       {propiedades?.map((propiedad, index) => (  
         <PropCard key={index} data={propiedad} />
-      ))}
+      ))} */}
     </div>
   );
 }
